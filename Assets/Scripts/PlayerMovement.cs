0110dcb85939b8714ae2a6c7b1e046a71566929d
raushan2ashish <raushan22@live.com>
2024-11-15 15:23:33 -0500

    Added animation to player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,9 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
+public enum Direction
+{
+    North,
+    East,
+    South,
+    West,
+}
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 3f;
+    public Animator walkAnimator;
+    Direction walkDirection = Direction.South;
 
     // Update is called once per frame
     void Update()
@@ -17,7 +26,38 @@ public class PlayerMovement : MonoBehaviour
 
         //Same as transform.position = transform.position + inputVector;
         transform.position += inputVector;
-
         
+        
+        PickanimationState(inputX, inputY);
+
+    }
+    void PickanimationState(float inputX, float inputY)
+    {
+        
+        if (inputX > 0)
+        {
+            walkDirection = Direction.East;
+        }
+        else if (inputX < 0)
+        {
+            walkDirection = Direction.West;
+        }
+        if (inputY > 0)
+        {
+            walkDirection = Direction.North;
+        }
+        else if (inputY < 0)
+        {
+            walkDirection = Direction.West;
+        }
+        if (inputX == 0 && inputY == 0)
+            { walkAnimator.speed = 0;
+        }
+        else
+        {
+            walkAnimator.speed = moveSpeed / 4;
+        }
+
+        walkAnimator.SetInteger("WalkDirection", (int)walkDirection);
     }
 }
