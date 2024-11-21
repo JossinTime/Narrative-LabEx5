80b9f91017583b4f95a5b872a403f1d5a9276a84
raushan2ashish <raushan22@live.com>
2024-11-21 13:51:47 -0500

    added journal dissapering

---
+++ Assets/Scripts/SpriteManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class SpriteManager : MonoBehaviour
+{
+    public GameObject journalSprite;
+    private void Start()
+    {
+        journalSprite.SetActive(true);
+    }
+
+    [YarnCommand("disable_journal_sprite")]
+    public void DisableJournalSprite()
+    {
+        if (journalSprite != null)
+        {
+            journalSprite.SetActive(false);
+        }
+    }
+
+    [YarnCommand("enable_journal_sprite")]
+    public void EnableJournalSprite()
+    {
+        if (journalSprite != null)
+        {
+            journalSprite.SetActive(true);
+        }
+    }
+}
